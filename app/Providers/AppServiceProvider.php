7f593e1ec8bc338ad5c5fbb9f54bcb796efa0d57
Elf Sundae <elf.sundae@gmail.com>
2016-10-24 23:29:16 +0800

    Register services in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,42 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         parent::register();
+
+        if ($this->app['config']['app.debug']) {
+            $this->registerServicesForDebugging();
+        }
+
+        if ($this->app->runningInConsole()) {
+            $this->registerServicesForConsole();
+        }
+
+        if (is_domain('admin')) {
+            $this->registerServicesForAdmin();
+        }
+    }
+
+    /**
+     * Register services for debugging.
+     */
+    protected function registerServicesForDebugging()
+    {
+        $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+    }
+
+    /**
+     * Register services for console.
+     */
+    protected function registerServicesForConsole()
+    {
+        $this->app->register(\BackupManager\Laravel\Laravel5ServiceProvider::class);
+    }
+
+    /**
+     * Register services for admin.
+     */
+    protected function registerServicesForAdmin()
+    {
+        $this->app->register(\Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class);
     }
 }
