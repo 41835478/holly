bdc0607143e1a6ee1cf7da3c531c083e8fb3a712
Elf Sundae <elf.sundae@gmail.com>
2016-10-27 02:03:13 +0800

    Publish captcha config

---
+++ config/captcha.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'characters' => '2346789abcdefghjmnpqrtuxyzABCDEFGHJMNPQRTUXYZ',
+
+    'default' => [
+        'length' => 4,
+        'width' => 80,
+        'height' => 32,
+        'quality' => 90,
+        'lines' => 0,
+        'bgImage' => true,
+        'fontColors' => ['#2c3e50', '#c0392b', '#16a085', '#c0392b', '#8e44ad', '#303f9f', '#f57c00', '#795548'],
+    ],
+
+];
