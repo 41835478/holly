19fa548eede3f125103fd71f5629e4b775178157
Elf Sundae <elf.sundae@gmail.com>
2016-10-28 03:35:21 +0800

    Add api home controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,4 @@
 
 Route::get('captcha/{config?}', '\Mews\Captcha\CaptchaController@getCaptcha');
 
-Route::get('/', function () {
-    return api('hello');
-});
+Route::any('/', 'HomeController@index');
