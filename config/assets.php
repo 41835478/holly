1fae1511d4e5acf67f525192b85bbf197d9686e3
Elf Sundae <elf.sundae@gmail.com>
2016-10-28 19:07:42 +0800

    Build assets

--- config/assets.php
+++ config/assets.php
@@ -1,7 +1,10 @@
 <?php
 
-return [
-    // 'css/app.css',
-    // 'js/app.js',
-    // 'img/app.png',
-];
+return array (
+  'favicon.ico' => 'd41d8cd98f',
+  'css/admin.css' => 'd5105ef4a8',
+  'css/site.css' => '81e20b087e',
+  'js/admin.js' => '8e5049d65f',
+  'js/site.js' => '70c000e8cc',
+  'js/ie-compatible.js' => 'b080ab765a',
+);
