8d72b03a1bd56476ff7ddcefc4836caa3d10b204
Elf Sundae <elf.sundae@gmail.com>
2016-11-02 03:19:38 +0800

    Update alerts blade

--- resources/views/includes/alerts.blade.php
+++ resources/views/includes/alerts.blade.php
@@ -7,24 +7,25 @@ $__alerts['danger'] = array_merge(
   (array) array_pull($__alerts, 'error')
 );
 $__alerts = array_filter($__alerts);
-
-foreach ($__alerts as $__alertType => $__alertMessage) {
-  if (is_array($__alertMessage)) {
-    $__alertMessage = '<ul>'.implode(array_map(function ($value) {
-      return '<li>'.htmlentities($value).'</li>';
-    }, $__alertMessage)).'</ul>';
-  } else {
-    $__alertMessage = string_value($__alertMessage);
-  }
-
-  $__alertMessage = trim(json_encode($__alertMessage), '"');
 ?>
+@if (count($__alerts))
 <script>
-  $(function () {
-    $("@yield('alerts-container')").first().bootnotify("{!! $__alertMessage !!}", "{{  $__alertType }}");
-  });
-</script>
+@foreach ($__alerts as $__alertType => $__alertMessage)
 <?php
+if (is_array($__alertMessage)) {
+  $__alertMessage = '<ul>'.implode(array_map(function ($value) {
+    return '<li>'.htmlentities($value).'</li>';
+  }, $__alertMessage)).'</ul>';
+} else {
+  $__alertMessage = string_value($__alertMessage);
 }
+
+$__alertMessage = trim(json_encode($__alertMessage), '"');
 ?>
+$(function () {
+  $("@yield('alerts-container')").first().bootnotify("{!! $__alertMessage !!}", "{{  $__alertType }}");
+});
+@endforeach
+</script>
+@endif
 @endif
