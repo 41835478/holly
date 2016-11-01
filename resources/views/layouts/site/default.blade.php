327d427a3c1c53b133a42e9b17991ec002257c6c
Elf Sundae <elf.sundae@gmail.com>
2016-11-02 03:36:16 +0800

    Add site/default.blade

---
+++ resources/views/layouts/site/default.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('wechat-icon', asset_url('img/icon-300.png'))
+@section('baidu-analytics', config('services.baidu_analytics'))
+@section('alerts-container', 'body')
+
+@push('css')
+  <link rel="stylesheet" href="{{ asset_url('css/site.css') }}">
+@endpush
+
+@push('js')
+<script src="{{ asset_url('js/site.js') }}"></script>
+@include('includes.alerts')
+@endpush
