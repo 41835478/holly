1a263587f4db45a4f61fbbe2f77d518dad9d9d7b
Elf Sundae <elf.sundae@gmail.com>
2016-11-02 23:35:36 +0800

    Add icons

--- config/assets.php
+++ config/assets.php
@@ -1,10 +1,12 @@
 <?php
 
 return array (
-  'favicon.ico' => 'd41d8cd98f',
+  'favicon.ico' => '4ed9d070ee',
   'css/admin.css' => '5fc4212da3',
   'css/site.css' => '81e20b087e',
   'js/admin.js' => '2f5ec084b1',
   'js/site.js' => 'f5fcd9ac2a',
   'js/ie-compatible.js' => 'b080ab765a',
+  'img/icon-120.png' => '808627bd43',
+  'img/icon-300.png' => 'b891380490',
 );
