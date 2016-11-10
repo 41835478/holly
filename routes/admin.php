630ce582d8bceb5125dbe86438c648a924f76f7c
Elf Sundae <elf.sundae@gmail.com>
2016-11-11 00:19:21 +0800

    Update admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -2,17 +2,30 @@
 
 Route::get('captcha/{config?}', '\Mews\Captcha\CaptchaController@getCaptcha');
 
-Route::group(['middleware' => 'guest'], function () {
-    Route::get('login', 'Auth\LoginController@showLoginForm');
-    Route::post('login', 'Auth\LoginController@login');
-    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::group(['namespace' => 'Auth'], function () {
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('login', 'LoginController@showLoginForm');
+        Route::post('login', 'LoginController@login');
+        Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm');
+        Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
+        Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm');
+        Route::post('password/reset', 'ResetPasswordController@reset');
+    });
+    Route::get('logout', 'LoginController@logout');
 });
-Route::get('logout', 'Auth\LoginController@logout');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', 'HomeController@index');
     Route::get('log', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+
+    Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+        Route::get('users', 'UserController@showUsers');
+        Route::get('users-data', 'UserController@getUsersData');
+        Route::post('user/create', 'UserController@createUser')
+            ->middleware('can:create,App\Models\AdminUser');
+        Route::post('user/delete/{admin_user}', 'UserController@deleteUser')
+            ->middleware('can:delete,admin_user');
+        Route::get('profile/{admin_user?}', 'ProfileController@show');
+        Route::post('profile/{admin_user?}', 'ProfileController@edit');
+    });
 });
