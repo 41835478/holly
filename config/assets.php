7cff4a12bddb5cb91b4db20b5b9bd7c0af5bfb94
Elf Sundae <elf.sundae@gmail.com>
2016-11-12 08:33:40 +0800

    Update assets

--- config/assets.php
+++ config/assets.php
@@ -2,10 +2,10 @@
 
 return array (
   'favicon.ico' => '4ed9d070ee',
-  'css/admin.css' => '66bb2b316d',
-  'css/site.css' => '24ebd809cd',
-  'js/admin.js' => '8bdb5c9ec7',
-  'js/site.js' => '5972c4066f',
+  'css/admin.css' => 'd37e790275',
+  'css/site.css' => 'efb1631622',
+  'js/admin.js' => '39d2b6efba',
+  'js/site.js' => '15a4b421ab',
   'js/ie-compatible.js' => 'b080ab765a',
   'img/icon-60.png' => '514a06dd1b',
   'img/icon-120.png' => '808627bd43',
