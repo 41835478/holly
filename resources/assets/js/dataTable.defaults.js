033531c0e61bee8e6a7aff1cb713b2b0e6766d89
Elf Sundae <elf.sundae@gmail.com>
2016-11-14 02:06:15 +0800

    Add dataTable.defaults.js

---
+++ resources/assets/js/dataTable.defaults.js
@@ -0,0 +1,11 @@
+;
+(function($) {
+  "use strict";
+
+  $.extend($.fn.dataTable.defaults, {
+    "processing": true,
+    "serverSide": true,
+    "lengthMenu": [20, 30, 50, 100],
+    "pageLength": 20
+  });
+})(jQuery);
