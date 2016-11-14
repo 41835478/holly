78fd8e8e0864bb4839d8ed486e5ba0712ac518b8
Elf Sundae <elf.sundae@gmail.com>
2016-11-15 03:41:50 +0800

    Add sprintf-js

--- gulpfile.js
+++ gulpfile.js
@@ -59,6 +59,7 @@ elixir((mix) => {
 function getScripts(scripts) {
   return [
     './node_modules/jquery/dist/jquery.js',
+    './node_modules/sprintf-js/src/sprintf.js',
     './node_modules/bootstrap/dist/js/bootstrap.js',
     './node_modules/holly-packages/utilities/csrf_token.js',
     './node_modules/holly-packages/utilities/captcha.js',
