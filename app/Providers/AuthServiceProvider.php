a461b6caf9a42da224cf1beaedc87af11778e066
Elf Sundae <elf.sundae@gmail.com>
2016-11-15 08:52:09 +0800

    Remove manager-system-log Gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,8 +25,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Gate::define('manage-system-log', function ($user) {
-            return $user instanceof \App\Models\AdminUser && $user->isSuperAdmin();
-        });
+        //
     }
 }
