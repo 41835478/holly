c00d65ec7087f648f8982b87c5a5d67b06b671a8
Elf Sundae <elf.sundae@gmail.com>
2016-11-16 21:08:38 +0800

    Rebuild assets

--- config/assets.php
+++ config/assets.php
@@ -2,10 +2,10 @@
 
 return array (
   'favicon.ico' => '4ed9d070ee',
-  'css/admin.css' => '4bf7ce7c7b',
-  'css/site.css' => '24ebd809cd',
-  'js/admin.js' => '7005a7ffca',
-  'js/site.js' => '76eef0a703',
+  'css/admin.css' => '84749e318c',
+  'css/site.css' => '082d7c6e74',
+  'js/admin.js' => 'a942da5584',
+  'js/site.js' => '5b965f18b4',
   'js/ie-compatible.js' => 'b080ab765a',
   'img/icon-60.png' => '514a06dd1b',
   'img/icon-120.png' => '808627bd43',
