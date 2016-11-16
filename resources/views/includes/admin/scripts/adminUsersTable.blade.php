65eba9aef196d41c4b21e80dd0db9ff928a41a60
Elf Sundae <elf.sundae@gmail.com>
2016-11-17 07:02:22 +0800

    use SweetAlert2 instead

--- resources/views/includes/admin/scripts/adminUsersTable.blade.php
+++ resources/views/includes/admin/scripts/adminUsersTable.blade.php
@@ -1,5 +1,5 @@
 <script>
-;(function ($) {
+(function ($) {
   $('#adminUsersTable tbody').on('click', '[class*="admin-user-action-"]', function (e) {
     e.preventDefault();
     let action = /admin-user-action-(\w+)/g.exec($(this).attr('class'))[1];
@@ -9,20 +9,20 @@
     if (action == 'edit') {
       location.href = '/admin/profile/'+data.id;
     } else if (action == 'delete') {
-      bootbox.confirm(
-        '<i class="fa fa-warning"></i> 确定删除管理员 <strong>'+data.username+'</strong> 吗？',
-        function (result) {
-          if (result) {
-            $.post('/admin/user/delete/'+data.id, function(json) {
-                if (json.code == 1) {
-                  table.ajax.reload();
-                } else {
-                  $('#adminUsersTable').closest('.box').bootnotifyApi(json, {position: "top"});
-                }
-            });
-          }
-        }
-      );
+      swal({
+        type: 'warning',
+        html: '确定删除管理员 <strong>'+data.username+'</strong> 吗？',
+        showCancelButton: true,
+        confirmButtonText: '确定删除'
+      }).then(function () {
+        $.post('/admin/user/delete/'+data.id, function(json) {
+            if (json.code == 1) {
+              table.ajax.reload(null, false);
+            } else {
+              swalApi(json);
+            }
+        });
+      });
     }
   });
 })(jQuery)
