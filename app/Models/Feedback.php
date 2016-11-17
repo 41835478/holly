b87653f759b3ca7c93d5a7938a5da47ec78f1a3c
Elf Sundae <elf.sundae@gmail.com>
2016-11-17 22:28:28 +0800

    Add code comments

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -6,23 +6,48 @@ use App\Exceptions\InvalidInputException;
 use Holly\Support\Helper;
 use Iatstuti\Database\Support\NullableFields;
 use Illuminate\Database\Eloquent\Model;
-use Request;
+use Illuminate\Support\Facades\Request;
 
 class Feedback extends Model
 {
     use NullableFields;
 
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
     public $timestamps = false;
 
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
     protected $appends = ['platform_string'];
 
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
     protected $dates = ['created_at'];
 
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
     protected $casts = [
         'user_id' => 'integer',
         'device_id' => 'integer',
     ];
 
+    /**
+     * The attributes that should be saved as null when empty.
+     *
+     * @var array
+     */
     protected $nullable = [
         'contact', 'user_id', 'device_id',
         'os_version', 'platform', 'network',
@@ -56,27 +81,25 @@ class Feedback extends Model
      * @param  array  $data
      * @param  int  $user_id
      * @param  int  $device_id
-     * @return static|null
+     * @return static
      *
      * @throws \App\Exceptions\InvalidInputException
      */
     public static function createFeedback($data, $user_id = null, $device_id = null)
     {
-        $content = trim(array_get($data, 'feedback_content'));
-
-        if (! is_string($content) || empty($content)) {
+        if (empty($content = trim(array_get($data, 'feedback_content')))) {
             throw new InvalidInputException('反馈内容不能为空！');
         }
 
         $contact = array_get($data, 'feedback_contact');
 
-        $data = array_only(array_filter($data), [
-            'os', 'os_version', 'platform', 'network',
-        ]) + compact('user_id', 'device_id', 'content', 'contact') + [
-            'os' => app('client')->os,
-            'ip' => Request::ip(),
-        ];
+        $attributes = array_only($data, ['os', 'os_version', 'platform', 'network'])
+            + compact('user_id', 'device_id', 'content', 'contact')
+            + [
+                'os' => app('client')->os,
+                'ip' => Request::ip(),
+            ];
 
-        return static::forceCreate($data);
+        return static::forceCreate($attributes);
     }
 }
