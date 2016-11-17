25a288e595dea16988833003075263d67dc587fa
Elf Sundae <elf.sundae@gmail.com>
2016-11-17 18:23:19 +0800

    Update admin index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -24,36 +24,5 @@
 
 @section('content')
 <div class="row">
-
-  <div class="col-md-3 col-sm-6 col-xs-12">
-    <div class="small-box stat-box bg-green">
-      <div class="inner">
-        <h3>8769</h3>
-        <p>今日新增用户</p>
-      </div>
-      <div class="icon">
-        <i class="fa fa-users"></i>
-      </div>
-      <a href="/user" class="small-box-footer">
-        总数：99888762 <i class="fa fa-arrow-circle-right"></i>
-      </a>
-    </div>
-  </div>
-
-  <div class="col-md-3 col-sm-6 col-xs-12">
-    <div class="small-box stat-box bg-light-blue">
-      <div class="inner">
-        <h3>8733</h3>
-        <p>今日新增设备</p>
-      </div>
-      <div class="icon">
-        <i class="fa fa-mobile"></i>
-      </div>
-      <a href="/device" class="small-box-footer">
-        总数：982273663 <i class="fa fa-arrow-circle-right"></i>
-      </a>
-    </div>
-  </div>
-
 </div>
 @stop
