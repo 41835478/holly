35200da65e60f258063d24c916e44f5b3568735a
Elf Sundae <elf.sundae@gmail.com>
2016-11-18 08:36:46 +0800

    UserDevice model

--- app/Models/UserDevice.php
+++ app/Models/UserDevice.php
@@ -2,31 +2,58 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserDevice extends Model
 {
     use SoftDeletes;
 
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
     protected $dates = ['deleted_at'];
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ['user_id', 'device_id'];
 
+    /**
+     * Touch UserDevice model.
+     *
+     * @param  int  $user_id
+     * @param  int  $device_id
+     * @return static|null
+     */
     public static function touchUserDevice($user_id, $device_id)
     {
         if ($user_id && $device_id) {
-            $instance = static::withTrashed()->firstOrNew(compact('user_id', 'device_id'));
+            $instance = static::withTrashed()
+                ->firstOrNew(compact('user_id', 'device_id'));
 
             if ($instance->trashed()) {
                 $instance->restore();
-            } else {
-                $instance->touch();
             }
+
+            $instance->touch();
 
             return $instance;
         }
     }
 
+    /**
+     * Fetch records.
+     *
+     * @param  int|null  $userId
+     * @param  int|null  $deviceId
+     * @param  bool $withTrashed
+     * @return mixed
+     */
     public static function findByUserDevice($userId = null, $deviceId = null, $withTrashed = false)
     {
         $query = static::query();
@@ -35,7 +62,6 @@ class UserDevice extends Model
             if (is_object($userId)) {
                 $userId = $userId->id;
             }
-
             $query->where('user_id', $userId);
         }
 
@@ -43,7 +69,6 @@ class UserDevice extends Model
             if (is_object($deviceId)) {
                 $deviceId = $deviceId->id;
             }
-
             $query->where('device_id', $deviceId);
         }
 
@@ -58,6 +83,13 @@ class UserDevice extends Model
         return $query->orderBy('updated_at', 'desc')->get();
     }
 
+    /**
+     * Delete model.
+     *
+     * @param  int  $userId
+     * @param  init  $deviceId
+     * @return bool|null
+     */
     public static function deleteUserDevice($userId, $deviceId)
     {
         if ($userId &&
