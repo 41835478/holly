b3e0fc5730466af23d5ab29f625733fcca19ec26
Elf Sundae <elf.sundae@gmail.com>
2016-11-22 06:26:37 +0800

    code comment

--- app/Http/Controllers/Site/Auth/LoginController.php
+++ app/Http/Controllers/Site/Auth/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login / registration.
+     * Where to redirect users after login.
      *
      * @var string
      */
