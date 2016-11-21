5257d6a299189b4f9f4afba9b33f30039771355c
Elf Sundae <elf.sundae@gmail.com>
2016-11-21 11:24:38 +0800

    Add AssetHelper trait

---
+++ app/Traits/AssetHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Storage;
+
+trait AssetHelper
+{
+    /**
+     * Get Filesystem instance for the given identifier.
+     *
+     * @param  string|null  $identifier
+     * @return \Illuminate\Contracts\Filesystem\Filesystem
+     */
+    protected function getFilesystem($identifier = null)
+    {
+        return Storage::disk(
+            method_exists($this, 'getFilesystemDisk') ? $this->getFilesystemDisk($identifier) : null
+        );
+    }
+
+    /**
+     * Get asset URL.
+     *
+     * @param  string  $path
+     * @param  string|null  $identifier
+     * @return string
+     */
+    protected function getAssetUrl($path, $identifier = null)
+    {
+        if (filter_var($path, FILTER_VALIDATE_URL) !== false) {
+            return $path;
+        }
+
+        return asset_url($this->getFilesystem($identifier)->url($path));
+    }
+}
