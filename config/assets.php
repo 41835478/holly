64b7fb1cb3bb7911fe5f30b8c24f94a89e79a122
Elf Sundae <elf.sundae@gmail.com>
2016-11-22 09:40:10 +0800

    Update assets config

--- config/assets.php
+++ config/assets.php
@@ -1,13 +1,13 @@
 <?php
 
-return array (
-  'favicon.ico' => '4ed9d070ee',
-  'css/admin.css' => 'e18fb2f092',
-  'css/site.css' => 'beaaf77079',
-  'js/admin.js' => '0748d445e2',
-  'js/site.js' => 'ac5c8668f9',
-  'js/ie-compatible.js' => 'b080ab765a',
-  'assets/icon-60.png' => '514a06dd1b',
-  'assets/icon-120.png' => '808627bd43',
-  'assets/icon-300.png' => 'b891380490',
-);
+return [
+    'favicon.ico',
+    'css/admin.css',
+    'css/site.css',
+    'js/ie-compatible.js',
+    'js/admin.js',
+    'js/site.js',
+    'assets/icon-60.png',
+    'assets/icon-120.png',
+    'assets/icon-300.png',
+];
