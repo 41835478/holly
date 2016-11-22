706bac6c440a2c63d32bafc9c53497ae23a25949
Elf Sundae <elf.sundae@gmail.com>
2016-11-22 22:41:34 +0800

    Add MagicPropertySetter trait

---
+++ app/Traits/MagicPropertySetter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+use InvalidArgumentException;
+
+trait MagicPropertySetter
+{
+    /**
+     * Handle dynamic method calls to set property.
+     *
+     * @param  string  $method
+     * @param  array $parameters
+     * @return $this
+     */
+    public function __call($method, $parameters)
+    {
+        if (property_exists($this, $method) && count($parameters) > 0) {
+            $this->{$method} = $parameters[0];
+        }
+
+        return $this;
+    }
+}
