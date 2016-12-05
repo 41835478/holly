aa69d303ce2d7277fc41abf1dc8ba727ef7f8777
Elf Sundae <elf.sundae@gmail.com>
2016-12-05 11:06:22 +0800

    Add optimus_encode and optimus_decode helper

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -196,3 +196,29 @@ if (! function_exists('api')) {
         return response()->api(...$args);
     }
 }
+
+if (! function_exists('optimus_encode')) {
+    /**
+     * Encode a number with Optimus.
+     *
+     * @param  int  $number
+     * @return int
+     */
+    function optimus_encode($number)
+    {
+        return app('optimus')->encode($number);
+    }
+}
+
+if (! function_exists('optimus_decode')) {
+    /**
+     * Decode a number with Optimus.
+     *
+     * @param  int  $number
+     * @return int
+     */
+    function optimus_decode($number)
+    {
+        return app('optimus')->decode($number);
+    }
+}
