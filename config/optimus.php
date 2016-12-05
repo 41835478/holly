6d80286a3ba88ac37f05f59f50b79bfd80d88e68
Elf Sundae <elf.sundae@gmail.com>
2016-12-05 09:22:52 +0800

    Add Optimus config

---
+++ config/optimus.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Optimus Connection
+    |--------------------------------------------------------------------------
+    |
+    | More info: https://github.com/jenssegers/optimus#usage
+    |
+    | - Large prime number lower than 2147483647
+    | - The inverse prime so that (PRIME * INVERSE) & MAXID == 1
+    | - A large random integer lower than 2147483647
+    |
+    */
+
+    'prime' => env('OPTIMUS_PRIME'),
+    'inverse' => env('OPTIMUS_INVERSE'),
+    'random' => env('OPTIMUS_RANDOM'),
+];
