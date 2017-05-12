a2356aad2e1c71d56f79e8e25f4cb04222e554be
Elf Sundae <elf.sundae@gmail.com>
2017-05-13 06:46:48 +0800

    Login, logout route name

--- routes/site.php
+++ routes/site.php
@@ -6,7 +6,7 @@ Route::get('/', 'HomeController@index');
 
 Route::group(['namespace' => 'Auth'], function () {
     Route::group(['middleware' => 'guest'], function () {
-        Route::get('login', 'LoginController@showLoginForm');
+        Route::get('login', 'LoginController@showLoginForm')->name('login');
     });
-    Route::get('logout', 'LoginController@logout');
+    Route::get('logout', 'LoginController@logout')->name('logout');
 });
