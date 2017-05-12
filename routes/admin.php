d4b944573118f0fcbf193cb58c4c7e9d8eb5b902
Elf Sundae <elf.sundae@gmail.com>
2017-05-12 20:24:32 +0800

    name routes

--- routes/admin.php
+++ routes/admin.php
@@ -4,14 +4,14 @@ Route::get('captcha/{config?}', '\Mews\Captcha\CaptchaController@getCaptcha');
 
 Route::group(['namespace' => 'Auth'], function () {
     Route::group(['middleware' => 'guest'], function () {
-        Route::get('login', 'LoginController@showLoginForm');
+        Route::get('login', 'LoginController@showLoginForm')->name('login');
         Route::post('login', 'LoginController@login');
-        Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm');
-        Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
-        Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm');
+        Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
         Route::post('password/reset', 'ResetPasswordController@reset');
     });
-    Route::get('logout', 'LoginController@logout');
+    Route::get('logout', 'LoginController@logout')->name('logout');
 });
 
 Route::group(['middleware' => 'auth'], function () {
