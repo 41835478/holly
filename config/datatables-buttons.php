e2a4146821433ee1219af03dec9e276652fab446
Elf Sundae <elf.sundae@gmail.com>
2017-06-09 06:20:33 +0800

    Add datatables stubs

--- config/datatables-buttons.php
+++ config/datatables-buttons.php
@@ -29,7 +29,7 @@ return [
     /**
      * Set Custom stub folder
      */
-    //'stub' => '/resources/custom_stub',
+    'stub' => '/resources/datatables-stubs',
 
     /**
      * PDF generator to be used when converting the table to pdf.
