7e8a4142f50ef1aceb523455b2a39af6ec80dc84
Elf Sundae <elf.sundae@gmail.com>
2017-06-09 23:25:07 +0800

    Revert assets version config file

--- config/assets.php
+++ config/assets.php
@@ -1,13 +1,13 @@
 <?php
 
-return array (
-  'favicon.ico' => '4ed9d070ee',
-  'css/admin.css' => 'c7ce1e2fda',
-  'css/site.css' => '9b84871f2c',
-  'js/ie-compatible.js' => 'b080ab765a',
-  'js/admin.js' => '34349f35ac',
-  'js/site.js' => '892e6b99f3',
-  'assets/icon-60.png' => '514a06dd1b',
-  'assets/icon-120.png' => '808627bd43',
-  'assets/icon-300.png' => 'b891380490',
-);
+return [
+    'favicon.ico',
+    'css/admin.css',
+    'css/site.css',
+    'js/ie-compatible.js',
+    'js/admin.js',
+    'js/site.js',
+    'assets/icon-60.png',
+    'assets/icon-120.png',
+    'assets/icon-300.png',
+];
