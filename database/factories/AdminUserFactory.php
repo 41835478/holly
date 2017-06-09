64a055aeab9efbc72eda156bd3cbe2de27da2cce
Elf Sundae <elf.sundae@gmail.com>
2017-06-09 16:28:18 +0800

    Change Faker imageUrl use Gravatar

--- database/factories/AdminUserFactory.php
+++ database/factories/AdminUserFactory.php
@@ -1,15 +1,16 @@
 <?php
 
 use App\Models\AdminUser;
+use App\Support\Helper;
 
 /* @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(AdminUser::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'email' => $faker->unique()->safeEmail,
+        'email' => $email = $faker->unique()->safeEmail,
         'username' => $faker->name,
-        'avatar' => asset_from($faker->imageUrl(640, 640), ''),
+        'avatar' => Helper::gravatar($email, 640),
         'password' => $password ?: $password = bcrypt(md5('secret')),
         'remember_token' => str_random(10),
     ];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,17 +1,18 @@
 <?php
 
 use App\Models\User;
+use App\Support\Helper;
 
 /* @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'email' => $faker->unique()->safeEmail,
+        'email' => $email = $faker->unique()->safeEmail,
         'phone' => $faker->unique()->phoneNumber,
         'password' => $password ?: $password = bcrypt(md5('secret')),
         'username' => $faker->userName,
-        'avatar' => $avatar = asset_from($faker->imageUrl(640, 640), ''),
+        'avatar' => $avatar = Helper::gravatar($email, 640),
         'original_avatar' => $avatar,
         'login_count' => $faker->numberBetween(1, 100000),
         'last_login_at' => $faker->dateTime(),
